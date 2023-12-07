574336289fbc7edfc43a95645d7507236d320967
seonghorang <psh6142@gmail.com>
2023-12-07 16:30:51 +0900

    [DAT] 음역대 측정 #5

--- mic_trans.py
+++ mic_trans.py
@@ -15,3 +15,23 @@ sd.wait()  # 녹음이 끝날 때까지 기다림
 plt.figure(figsize=(14, 5))
 plt.plot(myrecording)
 plt.show()
+
+
+##### 마이크 입력 후 시각화
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
+
+# 샘플링 레이트
+sr = 22050
+
+# 녹음 시간
+duration = 5  # seconds
+
+# 녹음 시작
+recording = sd.rec(int(duration * sr), samplerate=sr, channels=1)
+sd.wait()  # Wait until the recording is done
+
+# 녹음된 데이터 시각화
+plt.plot(recording)
+plt.show()
