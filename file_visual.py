54f76e40d7f75570e5a0ba5f4ee50fc953f6f2d9
seonghorang <psh6142@gmail.com>
2023-12-14 17:47:08 +0900

    [DAT] 노래 옥타브 조절 #9

--- file_visual.py
+++ file_visual.py
@@ -2,7 +2,7 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 
-y, sr = librosa.load('./voice/test1.wav')
+y, sr = librosa.load('./voice/그대만있다면.mp3')
 plt.figure(figsize=(14, 5))
 librosa.display.waveshow(y,alpha=0.4)
 plt.show()
