e90e537450a894f7aacbd51f8797a15408df6964
seonghorang <psh6142@gmail.com>
2024-01-28 15:00:07 +0900

    [FNT] 녹음 기능 구현

--- Django_Project/myapp/views.py
+++ Django_Project/myapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.conf import settings
 from .utils import handle_uploaded_file, calculate_frequency_range
@@ -7,6 +7,7 @@ from .forms import Uploadfileform
 import numpy as np
 import librosa
 import os
+import logging
 
 data_dir = os.path.join(settings.BASE_DIR, '..', 'data')
 
@@ -33,18 +34,46 @@ def handle_uploaded_file(f, request):
             destination.write(chunk)
 
     key = calculate_key(file_path)
-    FrequencyRange.objects.create(file_name=f.name, key=key)
+
+    if f.name == "recorded_audio.ogg":
+        FrequencyRange.objects.create(key=key)
+    else:
+        FrequencyRange.objects.create(file_name=f.name, key=key)
+
     request.session['key'] = key
-    request.session['file_name'] = f.name
+    request.session['file_name'] = f.name if f.name != "recorded_audio.ogg" else None
     return f.name, key
 
+def handle_audio_data(audio_data):
+    try:
+        file_name = 'recorded_audio.wav'
+        file_path = os.path.join(settings.BASE_DIR, file_name)
+        with open(file_path, 'wb') as f:
+            f.write(audio_data)
+        key = calculate_key(file_path)
+    except Exception as e:
+        logging.error(f"Error while handling audio data: {e}")
+        key = None
+    return key
+
 def range(request):
+    key = None
+    file_name = None
     if request.method == "POST":
         form = Uploadfileform(request.POST, request.FILES)
         if form.is_valid():
-            file_name, key = handle_uploaded_file(request.FILES['file'], request)
-            request.session['viewed'] = False
-            return HttpResponseRedirect('/range')
+            if 'file' in request.FILES:
+                file_name, key = handle_uploaded_file(request.FILES['file'], request)
+                request.session['viewed'] = False
+            elif 'audio' in request.FILES:
+                file_name, key = handle_uploaded_file(request.FILES['audio'], request)
+                request.session['key'] = key
+                request.session['file_name'] = file_name
+            else:
+                logging.warning("Form is not valid")
+            return JsonResponse({'key':key, 'file_name':file_name})
+        else:
+            logging.warning("Form is not valid")
     else:
         form = Uploadfileform()
         key = request.session.get('key')
